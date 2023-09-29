8a443a15fb0a2fbe165c0e9e60ca71374e1b54fa
Julia H. Lonn <julia.halvdansson.lonn@iths.se>
2023-09-29 08:56:58 +0200

    added container

--- src/components/ApiConnect.vue
+++ src/components/ApiConnect.vue
@@ -1,11 +1,14 @@
 <template>
     <div onclick="getCategories()">
-        <p>Categories:</p>
+        <div :class="categoryContainer">
+            <p>Categories:</p>
         <ul>
             <li v-for="category in list">
             <a href="#">{{ category.name }} ({{ category.count }})</a>
             </li>
         </ul>
+        </div>
+        
     </div>
 </template>
 
@@ -14,11 +17,14 @@
 export default {
     data() {
         return {
-            list: "test"
+            list: "test",
+            categoryContainer: "side-bar"
+    
         }
     },
     created() {
         this.getCategories()
+        
     },
     methods: {
         getCategories() {
@@ -27,9 +33,19 @@ export default {
                 .then((data) => { this.list = data })
                 .catch((error) => console.log(error));
         }
+        
     }
 }
 
 </script>
 
-<style scoped> a {color:black}</style>
+<style scoped> 
+a {color:black}
+.side-bar {
+background-color: blanchedalmond;
+width: 15%;
+height: 40%;
+border-radius: 10px;
+padding: 10px;
+}
+</style>
