c2a20ebe987151ff6c61b9147e8c6841391c583e
donxpedro <glody.mangumbu.don.pedro@iths.se>
2023-10-03 10:53:38 +0200

    add search

--- src/views/StartPage.vue
+++ src/views/StartPage.vue
@@ -1,11 +1,16 @@
 <script setup>
 import Card from '../components/CardComponent.vue'
-import ApiConnect from '../components/ApiConnect.vue';
+import ApiConnect from '../components/ApiConnect.vue'
+import SearchBarVue from '../components/Search.vue'
 </script>
 
 <template>
   <main>
+    <SearchBarVue></SearchBarVue>
     <ApiConnect></ApiConnect>
     <Card />
   </main>
+
 </template>
+
+
