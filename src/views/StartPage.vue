63d6409488943a2c5fce70f7585eae398656d781
Bashar Egho <bashar.egho@iths.com>
2023-10-03 10:12:27 +0200

    clean code and connect with Startpage router

--- src/views/StartPage.vue
+++ src/views/StartPage.vue
@@ -1,9 +1,11 @@
 <script setup>
 import Card from '../components/CardComponent.vue'
+import ApiConnect from '../components/ApiConnect.vue';
 </script>
 
 <template>
   <main>
+    <ApiConnect></ApiConnect>
     <Card />
   </main>
 </template>
