5e11112beccc1002cd32f5f0eababfda680c0173
Bashar Egho <bashar.egho@iths.com>
2023-10-06 12:56:17 +0200

    Make connect between card recipe and recipe page

---
+++ src/components/RecipeComponent.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+{{ recipe.title }}
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            recipe: {}
+        }
+    },
+    created() {
+        const recipeId = this.$route.params.id;
+        this.fetchItemById(recipeId)
+
+    },
+    methods: {
+        fetchItemById(id) {
+            fetch(`https://jau22-recept-grupp6-9v8e25zt13tu.reky.se/recipes/${id}`)
+                .then(response => response.json())
+                .then(data => this.recipe = data)
+                .catch(error => ("Error:", error));
+        },
+    }
+}
+</script>
+
+<style scoped></style>
