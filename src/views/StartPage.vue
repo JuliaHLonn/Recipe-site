789b30f27f1f46d36e9bded30161b55d06c6380c
Julia H. Lonn <julia.halvdansson.lonn@iths.se>
2023-10-06 10:29:39 +0200

    Send comment to database

--- src/views/StartPage.vue
+++ src/views/StartPage.vue
@@ -1,9 +1,11 @@
 <script setup>
 import Card from '../components/CardComponent.vue'
+import Comment from '../components/Comment.vue'
 
 </script>
 
 <template>
+    <Comment /> <!-- Ligger just nu här bara för att kunna testas! -->
     <Card />
 </template>
 
