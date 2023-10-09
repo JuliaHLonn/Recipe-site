983e9cb1b463a232c09772e72454333b9e7e6fe7
ellinorekmark <116821922+ellinorekmark@users.noreply.github.com>
2023-10-09 13:18:08 +0200

    disables/hides button when needed. Thanks message.

--- src/components/SetRating.vue
+++ src/components/SetRating.vue
@@ -1,18 +1,21 @@
 <template>
     <div class="setRatingBox">
         <div class="stars">
-            <span :class="{ active: hoverRating >= 1 }" @mouseover="hoverRating = 1" @mouseleave="hoverRating = clickedRating"
-                @click="clickedRating = 1">★</span>
-            <span :class="{ active: hoverRating >= 2 }" @mouseover="hoverRating = 2" @mouseleave="hoverRating = clickedRating"
-                @click="clickedRating = 2">★</span>
-            <span :class="{ active: hoverRating >= 3 }" @mouseover="hoverRating = 3" @mouseleave="hoverRating = clickedRating"
-                @click="clickedRating = 3">★</span>
-            <span :class="{ active: hoverRating >= 4 }" @mouseover="hoverRating = 4" @mouseleave="hoverRating = clickedRating"
-                @click="clickedRating = 4">★</span>
-            <span :class="{ active: hoverRating >= 5 }" @mouseover="hoverRating = 5" @mouseleave="hoverRating = clickedRating"
-                @click="clickedRating = 5">★</span>
+            <span :class="{ active: hoverRating >= 1 }" @mouseover="previewStars(1)"
+                @mouseleave="hoverRating = clickedRating" @click="clickedRating = 1">★</span>
+            <span :class="{ active: hoverRating >= 2 }" @mouseover="previewStars(2)"
+                @mouseleave="hoverRating = clickedRating" @click="clickedRating = 2">★</span>
+            <span :class="{ active: hoverRating >= 3 }" @mouseover="previewStars(3)"
+                @mouseleave="hoverRating = clickedRating" @click="clickedRating = 3">★</span>
+            <span :class="{ active: hoverRating >= 4 }" @mouseover="previewStars(4)"
+                @mouseleave="hoverRating = clickedRating" @click="clickedRating = 4">★</span>
+            <span :class="{ active: hoverRating >= 5 }" @mouseover="previewStars(5)"
+                @mouseleave="hoverRating = clickedRating" @click="clickedRating = 5">★</span>
         </div>
-        <button class="rateButton" @click="submitRating()">Betygsätt recept!</button>
+        <p v-if="ratingDone">Tack för ditt omdöme!</p>
+        <button v-else id="rateButton" @click="submitRating()" :disabled="buttonCheck" >Betygsätt recept!</button>
+
+
     </div>
 </template>
 
@@ -21,19 +24,24 @@ export default {
     data() {
         return {
             hoverRating: 0,
-            clickedRating: 1
+            clickedRating: 0,
+            ratingDone: false
         }
     },
     props: ['recipeId'],
     methods: {
+        previewStars(toHover) {
+            if (!this.ratingDone) {
+                this.hoverRating = toHover;
+            }
+        },
         submitRating() {
+            var button = document.getElementById("rateButton")
+            button.disabled = true;
             var url = "https://jau22-recept-grupp6-9v8e25zt13tu.reky.se/recipes/" + this.recipeId + "/ratings"
             var data = {
-                "rating": this.clickedRating
+                rating: this.clickedRating
             }
-            console.log(this.clickedRating)
-            console.log(this.recipeId)
-
             const response = fetch(url, {
                 method: "POST",
                 mode: "cors",
@@ -46,20 +54,59 @@ export default {
                 referrerPolicy: "no-referrer",
                 body: JSON.stringify(data),
             });
+            this.ratingDone = true;
+        }
+    },
+    computed:{
+        buttonCheck(){
+            if(this.clickedRating<=0){
+                return true;
+            }
+            else{
+                return false;
+            }
         }
     }
-
 }
 </script>
 
 <style scoped>
 .stars {
     font-size: 80px;
-    color: rgb(126, 126, 126);
+    color: rgb(172, 137, 169);
 }
 
 .active {
-    color: rgb(230, 194, 37);
-
+    color: rgb(255, 223, 80);
+}
+.setRatingBox{
+    border-radius: 10px;
+    border-width: 1px;
+    border-color:blueviolet;
+    border-style:solid;
+    box-sizing: border-box;
+    padding: 10px;
+    text-align: center;
+}
+#rateButton{
+    background-color:blueviolet;
+    border-color:blueviolet;
+    color:aliceblue;
+    font-family: Arial, Helvetica, sans-serif;
+    border-radius: 15px;
+    box-shadow: inset;
+    box-sizing: border-box;
+    padding: 10px;
+    font-size:large;
+}
+#rateButton:disabled{
+    background-color: rgb(223, 203, 221);
+    color:rgb(133, 120, 132);
+}
+#rateButton:hover{
+    background-color:rgb(173, 99, 241);
+}
+#rateButton:active{
+    background-color:rgb(120, 47, 189);
 }
 </style>
